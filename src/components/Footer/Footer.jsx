23a0d1c9e99f5f257574bfccad12fc7dbfd74713
Keegan Odell <Keegan.Odell@icloud.com>
2023-05-25 15:54:51 -0500

    hacked and slashed to create optimal start

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,11 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+	return (
+		<a href='https://github.com/Keegan-Odell'>
+			<footer>https://github.com/Keegan-Odell</footer>
+		</a>
+	);
 }
 
 export default Footer;
