b8ceb247e8e41c818e55274fb0bdc7803371f7fd
Keegan Odell <Keegan.Odell@icloud.com>
2023-05-30 21:41:38 -0500

    finished sidebar

--- src/components/MapPage/MapPage.jsx
+++ src/components/MapPage/MapPage.jsx
@@ -1,33 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MapPage.css';
+import DropDown from '../DropDown/DropDown';
 
 function MapPage() {
-	const handleMonthClick = (event) => {
-		event.preventDefault;
-		console.log('test');
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+	const lengths = [
+		'0-5',
+		'6-7',
+		'8-9',
+		'10-11',
+		'12-14',
+		'15-19',
+		'20-24',
+		'25-29',
+		'30-34',
+		'35+',
+	];
+	const typeOfFish = [
+		'Largemouth Bass',
+		'Smallmouth Bass',
+		'Walleye',
+		'Lake Trout',
+		'Northern Pike',
+		'Muskellunge',
+	];
+	const waterTemp = ['0-32', '33-40', '41-50', '51-60', '61-70', '71+'];
+
+	//title for menu buttons
+	let [monthTitle, setMonthTitle] = useState('Months');
+	let [lengthsTitle, setLengthsTitle] = useState('Length (inches)');
+	let [typeOfFishTitle, setTypeOfFishTitle] = useState('Type Of Fish');
+	let [waterTempTitle, setWaterTempTitle] = useState('Water Temperature (F°)');
+
+	let handleReset = () => {
+		setMonthTitle('Months');
+		setLengthsTitle('Length (inches)');
+		setTypeOfFishTitle('Type Of Fish');
+		setWaterTempTitle('Water Temperature (F°)');
+		window.location.reload(true);
 	};
 
 	return (
 		<div>
-			<div className='sideBar'>
-				<div className='dropdown'>
-					<button className='dropbtn'>Months</button>
-					<div className='dropdown-content'>
-						<button className='monthBtn'>January</button>
-						<button className='monthBtn'>February</button>
-						<button className='monthBtn'>March</button>
-						<button className='monthBtn'>April</button>
-						<button className='monthBtn'>May</button>
-						<button className='monthBtn'>June</button>
-						<button className='monthBtn'>July</button>
-						<button className='monthBtn'>August</button>
-						<button className='monthBtn'>September</button>
-						<button className='monthBtn'>October</button>
-						<button className='monthBtn'>November</button>
-						<button className='monthBtn'>December</button>
-					</div>
-				</div>
-			</div>
+			<div className='title'>Fish Finder</div>
+			<DropDown info={months} title={monthTitle} />
+			<DropDown info={lengths} title={lengthsTitle} />
+			<DropDown info={typeOfFish} title={typeOfFishTitle} />
+			<DropDown info={waterTemp} title={waterTempTitle} />
+			<button>Search</button>
+			<button onClick={handleReset}>Reset</button>
+			<button>Add New Fish</button>
 		</div>
 	);
 }
