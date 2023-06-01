58affdf3eaf5a0e82e88f73738b6ee3d51200ba2
Keegan Odell <Keegan.Odell@icloud.com>
2023-06-01 09:47:12 -0500

    added rest of tables to sql file, and added test data

--- database.sql
+++ database.sql
@@ -8,3 +8,33 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "catch" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" NOT NULL,
+	"fish_id" INT REFERENCES "fish",
+	"month" VARCHAR(80),
+	"latitude" DECIMAL NOT NULL,
+	"longitude" DECIMAL NOT NULL,
+	"length" INTEGER,
+	"water_temp" INTEGER 
+)
+
+CREATE TABLE "fish" (
+	"id" SERIAL PRIMARY KEY,
+  	"name" VARCHAR(80) NOT NULL
+)
+
+INSERT INTO "fish" ("name")
+VALUES
+('Largemouth_Bass'),
+('Smallmouth_Bass'),
+('Walleye'),
+('Lake_Trout'),
+('Northern_Pike'),
+('Muskellunge')
+
+-- DUMMY TEST DATA
+INSERT INTO "catch" ("user_id", "fish_id", "month", "latitude", "longitude", "length", "water_temp")
+VALUES
+(1, 1, 'august', '47.418', '-93.507', '24', '65')
